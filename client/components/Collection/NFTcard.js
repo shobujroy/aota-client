332adc8ee4afbba25f4aafe04052560fbe341470
Shafin The Developer <sahariar350@gmail.com>
2022-05-27 22:28:16 +0600

    done

--- client/components/Collection/NFTcard.js
+++ client/components/Collection/NFTcard.js
@@ -1,14 +1,37 @@
+import axios from 'axios';
 import Image from 'next/image'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function NFTcard({ src, title }) {
+function NFTcard({ api, src, title }) {
+    const [nft, setNft] = useState(null);
+
+    const getData = async (api) => {
+        console.log("working", api)
+        const something = api.split('/');
+        console.log(`https://ipfs.io/ipfs/${ something[2] }/${ something[3] }`)
+        try {
+            const { data } = await axios.get(`https://ipfs.io/ipfs/${ something[2] }/${ something[3] }`);
+            setNft(data);
+            console.log(data, 'data');
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    useEffect(() => {
+        getData(api);
+    }, [])
     return (
-        <div class="card" style={{ width: "18rem" }}>
-            <Image src={src} class="card-img-top img-fit" alt={title} />
-            <div class="card-body">
-                <h5 class="card-title">{title}</h5>
-            </div>
-        </div>
+        <>
+            {nft && (
+                <div class="card" style={{ width: "18rem", marginTop: "15px" }}>
+                    <Image src={`https://ipfs.io/ipfs/${nft.image.split('/')[2]}/${nft.image.split('/')[3]}`} width={300} height={300} class="card-img-top img-fit" alt={title} />
+                    <div class="card-body">
+                        <h5 class="card-title">{nft.name}</h5>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
 
--- client/context/MintContext.js
+++ client/context/MintContext.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import Web3 from "web3";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import Web3Modal from "web3modal";
+import swal from 'sweetalert';
 
 export const MintContext = React.createContext();
 
@@ -37,33 +38,21 @@ export const MintProvider = ({ children }) => {
 
     // connect wallet
     async function connectwallet() {
-        console.log(typeof window.ethereum !== "undefined", "worigi")
+        
         if (typeof window.ethereum !== "undefined") {
             try {
                 const provider = await web3Modal.connect();
-                console.log("provider from connectwallet function in Navbar");
-                console.log(provider);
 
                 const web3 = new Web3(provider);
-                console.log("web3 from connectwallet function in Navbar");
-                console.log(web3);
 
                 setIsConnected(true);
-                console.log("isConnected from connectwallet function in Navbar");
-                console.log(isConnected);
 
                 setSign((await web3.eth.getAccounts())[0]);
-                console.log("sign from connectwallet function in Navbar");
-                console.log(sign);
             } catch (e) {
-                console.log("error from connectwallet function in Navbar");
-                console.log(e);
+                
             }
         } else {
-            console.log("coming point 3")
             setIsConnected(false);
-            console.log("isConnected from connectwallet function in Navbar");
-            console.log(isConnected);
         }
     }
 
@@ -72,12 +61,12 @@ export const MintProvider = ({ children }) => {
         await web3Modal.clearCachedProvider();
         setSign(undefined);
         //setSign(undefined);
-        console.log("sign from disconnectwallet function in Navbar");
-        console.log(sign);
+        
+        
 
         setIsConnected(false);
-        console.log("isConnected from disconnectwallet function in Navbar");
-        console.log(isConnected);
+        
+        
         //setHasMetamask(false);
     }
 
@@ -90,49 +79,33 @@ export const MintProvider = ({ children }) => {
                 providerOptions,
             });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
-
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
-
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
-
             const PUB_PRICE = await Cont.methods.pubPrice().call();
-            console.log("reading pub_price from pubMint function in product card");
-            console.log(PUB_PRICE);
-
             let total = count * PUB_PRICE
-            console.log("total");
-            console.log(total);
-
             let price = web3.utils.fromWei(total.toString(), "ether");
-            console.log("calculated price in ether from pubMint function of product card");
-            console.log(price);
-
             setPubPrice(price);
-            console.log("pubPrice from pubMint function in product card");
-            console.log(pubPrice);
-
             try {
                 await Cont.methods.mintPubNFTs(count).send({ from: sign, value: total }, function (err, txHash) {
                     if (err) {
-                        console.log("error from results of send transaction of pubMint function in product card");
-                        console.log(err);
+                        
+                        
                     } else {
-                        console.log("txHash from pubMint function in product card");
+                        
                         setTrxHash(txHash);
+                        swal({
+                            title: "You have minted successfully!",
+                            text: `This is your hash of transaction: ${ txHash }`,
+                            icon: "success",
+                        });
                     }
                 });
             } catch (error) {
-                console.log("error from mint transaction of pubMint function in product card");
-                console.log(error);
+                
+                
             }
         } else {
-            console.log("Please have the right amount of money");
+            
         }
     }
 
@@ -145,83 +118,68 @@ export const MintProvider = ({ children }) => {
                 providerOptions,
             });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
-
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
 
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
 
             const PRI_PRICE = await Cont.methods.priPrice().call();
-            console.log("reading pub_price from pubMint function in product card");
-            console.log(PRI_PRICE);
 
             let total = count * PRI_PRICE
-            console.log("total");
-            console.log(total);
 
             let price = web3.utils.fromWei(total.toString(), "ether");
-            console.log("calculated price in ether from pubMint function of product card");
-            console.log(price);
-
-            //setPubPrice(price);
-            //console.log("pubPrice from pubMint function in product card");
-            //console.log(pubPrice);
 
             try {
                 await Cont.methods.mintPriNFTs(count).send({ from: sign, value: total }, function (err, txHash) {
                     if (err) {
-                        console.log("error from results of send transaction of pubMint function in product card");
-                        console.log(err);
+                        
+                        
                     } else {
-                        console.log("txHash from pubMint function in product card");
+                        
                         setTrxHash(txHash);
                     }
                 });
             } catch (error) {
-                console.log("error from mint transaction of pubMint function in product card");
-                console.log(error);
+                
+                
             }
         } else {
-            console.log("Please have the right amount of money");
+            
         }
     }
 
     // reserve mint
     async function reserve() {
         if (typeof window.ethereum !== "undefined") {
+            web3Modal = new Web3Modal({
+                cacheProvider: true,
+                providerOptions,
+            });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
 
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
 
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
 
             try {
                 await Cont.methods.reserveNFTs().send({ from: sign }, function (err, txHash) {
                     if (err) {
-                        console.log("error from results of send transaction of pubMint function in product card");
-                        console.log(err);
+                        
+                        
                     } else {
-                        console.log("txHash from pubMint function in product card");
-                        console.log(txHash);
+                        // swal
+                        swal({
+                            title: "You have minted successfully!",
+                            text: `This is your hash of transaction: ${ txHash }`,
+                            icon: "success",
+                        });
                     }
                 });
             } catch (error) {
-                console.log("error from mint transaction of pubMint function in product card");
-                console.log(error);
+                
+                
             }
         } else {
-            console.log("Please have the right amount of money");
+            
         }
     }
 
@@ -233,33 +191,27 @@ export const MintProvider = ({ children }) => {
                 providerOptions,
             });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
 
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
 
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
 
             try {
                 await Cont.methods.withdraw().send({ from: sign }, function (err, txHash) {
                     if (err) {
-                        console.log("error from results of send transaction of pubMint function in product card");
-                        console.log(err);
+                        
+                        
                     } else {
-                        console.log("txHash from pubMint function in product card");
-                        console.log(txHash);
+                        
+                        
                     }
                 });
             } catch (error) {
-                console.log("error from mint transaction of pubMint function in product card");
-                console.log(error);
+                
+                
             }
         } else {
-            console.log("Please have the right amount of money");
+            
         }
     }
 
@@ -272,33 +224,27 @@ export const MintProvider = ({ children }) => {
                 providerOptions,
             });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
 
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
 
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
 
             try {
                 await Cont.methods.safeTransfer(reciverAdd, tokenId).send({ from: sign }, function (err, txHash) {
                     if (err) {
-                        console.log("error from results of send transaction of pubMint function in product card");
-                        console.log(err);
+                        
+                        
                     } else {
-                        console.log("txHash from pubMint function in product card");
+                        
                         setTrxHash(txHash);
                     }
                 });
             } catch (error) {
-                console.log("error from mint transaction of pubMint function in product card");
-                console.log(error);
+                
+                
             }
         } else {
-            console.log("Please have the right amount of money");
+            
         }
     }
 
@@ -311,22 +257,17 @@ export const MintProvider = ({ children }) => {
                 providerOptions,
             });
             const provider = await web3Modal.connect();
-            console.log("provider from connect function in product card");
-            console.log(provider);
 
             const web3 = new Web3(provider);
-            console.log("web3 from connect function in product card");
-            console.log(web3);
 
             const Cont = new web3.eth.Contract(AOTA.abi, add, sign);
-            console.log("contruct intance from pubMint function in product card");
-            console.log(Cont);
 
             try {
                 const nftURI = await Cont.methods.getMyNFTs().call({ from: sign });
-                console.log('nftURI', nftURI);
+                
+                setCollection(nftURI)
             } catch (error) {
-                console.log(error);
+                
             }
         };
     }
@@ -334,7 +275,6 @@ export const MintProvider = ({ children }) => {
     useEffect(() => {
         if (typeof window.ethereum !== "undefined") {
             setHasMetamask(false);
-            console.log("hasMetamask from useEffect of index.js of public-sale page");
         }
     }, [hasMetamask]);
     return (
