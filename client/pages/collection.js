3daadfa2d7264ecc01c1648d9f101ad0ead8fa67
CoderMohammadShuvo <mohammad.shuvo4099@outlook.com>
2022-06-25 01:04:42 +0600

    gallary page mob height fixed, roadmap page arrow button fixed, collection, mint and success page background and style changed

--- client/pages/collection.js
+++ client/pages/collection.js
@@ -1,34 +1,32 @@
-import React, { useContext, useEffect } from 'react'
-import { MintContext } from './../context/MintContext';
-import Navbar from './../components/Shared/Navbar/Navbar';
-import Footer from './../components/Shared/Footer/Footer';
-import NFTcard from './../components/Collection/NFTcard';
+import React, { useContext, useEffect } from "react";
+import { MintContext } from "./../context/MintContext";
+import Navbar from "./../components/Shared/Navbar/Navbar";
+import Footer from "./../components/Shared/Footer/Footer";
+import NFTcard from "./../components/Collection/NFTcard";
 
 function Collection() {
-    const { myNFTs, collection, isConnected, hasMetamask, sign, } = useContext(MintContext);
+  const { myNFTs, collection, isConnected, hasMetamask, sign } =
+    useContext(MintContext);
 
-
-    useEffect(() => {
-        myNFTs();
-    }, [isConnected, hasMetamask, sign]);
-    return (
-        <div className='bg-third'>
-            <Navbar BorderBottom={true} wallet={true} />
-            <div className="container pt-3 pb-5">
-                <div className="row">
-                    <h1 className='fw-bold'>Collection</h1>
-                    <div className='mt-3 d-flex justify-content-between flex-wrap justify-content-sm-center'>
-                        {collection.map((item, index) => {
-                            return (
-                                <NFTcard api={item} />
-                            )
-                        })}
-                    </div>
-                </div>
-            </div>
-            <Footer bg={'footer-bg-secondary'} />
+  useEffect(() => {
+    myNFTs();
+  }, [isConnected, hasMetamask, sign]);
+  return (
+    <div style={{ backgroundColor: "#051509" }}>
+      <Navbar BorderBottom={true} wallet={true} />
+      <div className="container pt-3 pb-5">
+        <div className="row">
+          <h1 className="fw-bold">Collection</h1>
+          <div className="mt-3 d-flex justify-content-between flex-wrap justify-content-sm-center">
+            {collection.map((item, index) => {
+              return <NFTcard api={item} />;
+            })}
+          </div>
         </div>
-    )
+      </div>
+      <Footer bg={"footer-bg-secondary"} />
+    </div>
+  );
 }
 
-export default Collection
+export default Collection;
--- client/pages/public-sale/index.js
+++ client/pages/public-sale/index.js
@@ -1,30 +1,30 @@
-import React, { useContext } from 'react'
-import Navbar from '../../components/Shared/Navbar/Navbar';
-import Footer from '../../components/Shared/Footer/Footer';
-import ProductCard from '../../components/PublicSale/ProductCard/ProductCard';
+import React, { useContext } from "react";
+import Navbar from "../../components/Shared/Navbar/Navbar";
+import Footer from "../../components/Shared/Footer/Footer";
+import ProductCard from "../../components/PublicSale/ProductCard/ProductCard";
 import { useState, useEffect } from "react";
-import { MintContext } from './../../context/MintContext';
+import { MintContext } from "./../../context/MintContext";
 
 function Index() {
-    const { isConnected, hasMetamask, sign, } = useContext(MintContext);
+  const { isConnected, hasMetamask, sign } = useContext(MintContext);
 
-    return (
-        <div className='bg-third'>
-            <Navbar BorderBottom={true} wallet={true}  />
-            <div className="container pt-3 pb-5">
-                <div className="row">
-                    <h6 className='d-flex flex-row align-items-center'>
-                        <i className="fa-solid fa-arrow-left-long me-2"></i>
-                        Back
-                    </h6>
-                    <div className='mt-3 d-flex justify-content-center'>
-                        <ProductCard />
-                    </div>
-                </div>
-            </div>
-            <Footer bg={'footer-bg-secondary'} />
+  return (
+    <div style={{ backgroundColor: "#051509" }}>
+      <Navbar BorderBottom={true} wallet={true} />
+      <div className="container pt-3 pb-5">
+        <div className="row">
+          <h6 className="d-flex flex-row align-items-center">
+            <i className="fa-solid fa-arrow-left-long me-2"></i>
+            Back
+          </h6>
+          <div className="mt-3 d-flex justify-content-center">
+            <ProductCard />
+          </div>
         </div>
-    )
+      </div>
+      <Footer bg={"footer-bg-secondary"} />
+    </div>
+  );
 }
 
-export default Index
+export default Index;
