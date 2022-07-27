ecd7c21f48b1ce3a7112df470c2c8a51a36ef6d3
Ifeanyichukwu Welechi <welechi@icloud.com>
2022-07-27 11:58:37 +0100

    sc update

--- smart_contract/contracts/AOTA.sol
+++ smart_contract/contracts/AOTA.sol
@@ -1,10 +1,10 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract AOTA is ERC721Enumerable, Ownable {
     using SafeMath for uint256;
@@ -12,81 +12,108 @@ contract AOTA is ERC721Enumerable, Ownable {
 
     Counters.Counter private _tokenIds;
 
-    uint public constant MAX_SUPPLY = 7146;
-    uint public constant PUB_PRICE = 0.07 ether;
-    uint public constant PRI_PRICE = 0.09 ether;
-    uint public constant MAX_PER_MINT = 40;
+    string private _mockImageLink;
+    uint256 private _deployBlockTimestamp;
+
+    uint256 public constant MAX_SUPPLY = 7146;
+    uint256 public constant PUB_PRICE = 0.07 ether;
+    uint256 public constant PRI_PRICE = 0.09 ether;
+    uint256 public constant MAX_PER_MINT = 146;
 
     string public baseTokenURI;
 
-    constructor(string memory baseURI) ERC721("Aliens On The Ave", "AOTA") {
+    constructor(string memory baseURI, string memory mockImageLink)
+        ERC721("Aliens On The Ave", "AOTA")
+    {
         setBaseURI(baseURI);
+        _mockImageLink = mockImageLink;
+        _deployBlockTimestamp = block.timestamp;
     }
 
-    function pubPrice() public pure returns(uint) {
+    function pubPrice() public pure returns (uint256) {
         return PUB_PRICE;
     }
 
-    function priPrice() public pure returns(uint) {
+    function priPrice() public pure returns (uint256) {
         return PRI_PRICE;
     }
 
     function reserveNFTs() public onlyOwner {
-        uint totalMinted = _tokenIds.current();
+        uint256 totalMinted = _tokenIds.current();
 
-        require(totalMinted.add(40) < MAX_SUPPLY, "Not enough NFTs left to reserve");
+        require(
+            totalMinted.add(146) < MAX_SUPPLY,
+            "Not enough NFTs left to reserve"
+        );
 
-        for (uint i = 0; i < 40; i++) {
+        for (uint256 i = 0; i < 146; i++) {
             _mintSingleNFT();
         }
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return baseTokenURI;
+        if (block.timestamp > _deployBlockTimestamp + 2 weeks) {
+            return baseTokenURI;
+        } else {
+            return _mockImageLink;
+        }
     }
 
     function setBaseURI(string memory _baseTokenURI) public onlyOwner {
         baseTokenURI = _baseTokenURI;
     }
 
-    function mintPubNFTs(uint _count) public payable {
-        uint totalMinted = _tokenIds.current();
+    function mintPubNFTs(uint256 _count) public payable {
+        uint256 totalMinted = _tokenIds.current();
 
         require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
-        require(_count >0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
-        require(msg.value >= PUB_PRICE.mul(_count), "Not enough ether to purchase NFTs.");
+        require(
+            _count > 0 && _count <= MAX_PER_MINT,
+            "Cannot mint specified number of NFTs."
+        );
+        require(
+            msg.value >= PUB_PRICE.mul(_count),
+            "Not enough ether to purchase NFTs."
+        );
 
         payable(owner()).transfer(msg.value);
 
-
-        for (uint i = 0; i < _count; i++) {
+        for (uint256 i = 0; i < _count; i++) {
             _mintSingleNFT();
         }
     }
 
-    function mintPriNFTs(uint _count) public payable {
-        uint totalMinted = _tokenIds.current();
+    function mintPriNFTs(uint256 _count) public payable {
+        uint256 totalMinted = _tokenIds.current();
 
         require(totalMinted.add(_count) <= MAX_SUPPLY, "Not enough NFTs left!");
-        require(_count >0 && _count <= MAX_PER_MINT, "Cannot mint specified number of NFTs.");
-        require(msg.value >= PRI_PRICE.mul(_count), "Not enough ether to purchase NFTs.");
+        require(
+            _count > 0 && _count <= MAX_PER_MINT,
+            "Cannot mint specified number of NFTs."
+        );
+        require(
+            msg.value >= PRI_PRICE.mul(_count),
+            "Not enough ether to purchase NFTs."
+        );
 
         payable(owner()).transfer(msg.value);
 
-
-        for (uint i = 0; i < _count; i++) {
+        for (uint256 i = 0; i < _count; i++) {
             _mintSingleNFT();
         }
     }
 
     function _mintSingleNFT() private {
-        uint newTokenID = _tokenIds.current();
+        uint256 newTokenID = _tokenIds.current();
         _safeMint(msg.sender, newTokenID);
         _tokenIds.increment();
     }
 
     function safeTransfer(address to, uint256 tokenId) public {
-        require(_isApprovedOrOwner(_msgSender(), tokenId), "AOTA::SafeTransfer: transfer caller is not owner");
+        require(
+            _isApprovedOrOwner(_msgSender(), tokenId),
+            "AOTA::SafeTransfer: transfer caller is not owner"
+        );
         _safeTransfer(msg.sender, to, tokenId, "");
     }
 
@@ -110,5 +137,4 @@ contract AOTA is ERC721Enumerable, Ownable {
 
         return tokenURIs;
     }
-
 }
