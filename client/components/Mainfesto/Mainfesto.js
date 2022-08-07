413b147ee3c958255c682dc18739d4aabd3206ba
Shafin The Developer <sahariar350@gmail.com>
2022-08-07 19:41:34 +0600

    done

--- client/components/Mainfesto/Mainfesto.js
+++ client/components/Mainfesto/Mainfesto.js
@@ -1,13 +1,11 @@
 import Navbar from "../Shared/Navbar/Navbar";
 import Styles from "./Mainfesto.module.css";
 import MainfestoHero from "./MainFestoHero/MainfestoHero";
-import Footer from './../Shared/Footer/Footer';
 const MainFesto = () => {
   return (
     <div className={Styles.mainFesot}>
       <Navbar />
       <MainfestoHero />
-      {/* <Footer bg={"bg-main"} /> */}
     </div>
   );
 };
