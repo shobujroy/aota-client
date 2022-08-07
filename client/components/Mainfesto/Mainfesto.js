97898b05d215d6fbc6c9fa3349adb9dc50ee3e98
Shafin The Developer <sahariar350@gmail.com>
2022-08-07 12:42:02 +0600

    Added footer component at mainfesto

--- client/components/Mainfesto/Mainfesto.js
+++ client/components/Mainfesto/Mainfesto.js
@@ -1,11 +1,13 @@
 import Navbar from "../Shared/Navbar/Navbar";
 import Styles from "./Mainfesto.module.css";
 import MainfestoHero from "./MainFestoHero/MainfestoHero";
+import Footer from './../Shared/Footer/Footer';
 const MainFesto = () => {
   return (
     <div className={Styles.mainFesot}>
       <Navbar />
       <MainfestoHero />
+      <Footer bg={"bg-main"} />
     </div>
   );
 };
