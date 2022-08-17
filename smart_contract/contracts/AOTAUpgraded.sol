4c972af4cd032abb07a022f6f6e309499f4ed154
Ifeanyichukwu Welechi <welechi@icloud.com>
2022-08-17 17:03:28 +0100

    Update AOTAUpgraded.sol

--- smart_contract/contracts/AOTAUpgraded.sol
+++ smart_contract/contracts/AOTAUpgraded.sol
@@ -19,6 +19,7 @@ contract AOTAUpgraded is ERC721r, Ownable {
     mapping(address => bool) public isWhitelisted;
 
     string public baseTokenURI;
+    string public baseExtension = ".json";
 
     enum SalesStatus {
         NULL,
@@ -313,5 +314,30 @@ contract AOTAUpgraded is ERC721r, Ownable {
                 isWhitelisted[listOfUsers[i]] = true;
             }
         }
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        require(
+            _exists(tokenId),
+            "ERC721Metadata: URI query for nonexistent token"
+        );
+
+        string memory currentBaseURI = _baseURI();
+        return
+            bytes(currentBaseURI).length > 0
+                ? string(
+                    abi.encodePacked(
+                        currentBaseURI,
+                        tokenId.toString(),
+                        baseExtension
+                    )
+                )
+                : "";
     }
 }
