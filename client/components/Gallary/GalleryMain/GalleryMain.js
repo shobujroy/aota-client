82c27f3d215b6b75cc6a2e87b79851f759eb17c8
SAMIUR <samiurkhan94@gmail.com>
2022-09-07 00:24:41 +0600

    golden mode disabled

--- client/components/Gallary/GalleryMain/GalleryMain.js
+++ client/components/Gallary/GalleryMain/GalleryMain.js
@@ -1,59 +1,64 @@
 import Image from "next/image";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import { data } from "../data";
 import { Item } from "./GallaryImage";
 import Styles from "./GalleryMain.module.css";
-import axios from 'axios'
+import axios from "axios";
 
 const label = { inputProps: { "aria-label": "Switch demo" } };
 
 const GalleryMain = () => {
-  const [filter,setFilter] = useState({})
-  const [galleryData,setGalleryData] = useState([]);
+  const [filter, setFilter] = useState({});
+  const [galleryData, setGalleryData] = useState([]);
   const [info, setInfo] = useState(data);
   const [modalOpen, setModalOpen] = useState(null);
-  async function fetchData(){
-    const allData= await Promise.all(
-      Item.map(async item => {
-        const {data} = await axios.get(`https://aota.mypinata.cloud/ipfs/QmNd9KBk8NAqrZuCZjysmnoX5Hb9h26cSZVX86KecfGFNY/${item}.json`)
-        return data
+  async function fetchData() {
+    const allData = await Promise.all(
+      Item.map(async (item) => {
+        const { data } = await axios.get(
+          `https://aota.mypinata.cloud/ipfs/QmNd9KBk8NAqrZuCZjysmnoX5Hb9h26cSZVX86KecfGFNY/${item}.json`
+        );
+        return data;
       })
-    )
-    return allData
+    );
+    return allData;
   }
-  function handleFilter({name},option){
-    const newCat = {}
-    newCat[`${name}`] = option
+  function handleFilter({ name }, option) {
+    const newCat = {};
+    newCat[`${name}`] = option;
     setFilter((prevState) => ({
-      ...prevState, 
-      ...newCat
-    }))
+      ...prevState,
+      ...newCat,
+    }));
   }
-  async function filterData(filter){
-    const filteredContent = []
-    const originalData = await fetchData()
-    originalData.forEach(dataSet =>{
-      dataSet.attributes.forEach(attr => {
-        if(attr.trait_type in filter && filter[`${attr.trait_type}`] === attr.value){
-          filteredContent.push(dataSet)
+  async function filterData(filter) {
+    const filteredContent = [];
+    const originalData = await fetchData();
+    originalData.forEach((dataSet) => {
+      dataSet.attributes.forEach((attr) => {
+        if (
+          attr.trait_type in filter &&
+          filter[`${attr.trait_type}`] === attr.value
+        ) {
+          filteredContent.push(dataSet);
         }
-      })
-    })
-    if (filteredContent.length){
-      setGalleryData(filteredContent)
+      });
+    });
+    if (filteredContent.length) {
+      setGalleryData(filteredContent);
     }
   }
-  async function handleReset(){
-    setGalleryData(await fetchData())
+  async function handleReset() {
+    setGalleryData(await fetchData());
   }
 
   useEffect(() => {
-    (async () => setGalleryData(await fetchData()))()
-  },[])
+    (async () => setGalleryData(await fetchData()))();
+  }, []);
   //Needed this effect cause state concurrency is a bitch, but there is a room for improvement
   useEffect(() => {
-    filterData(filter)
-  },[filter])
+    filterData(filter);
+  }, [filter]);
 
   const toggle = (i) => {
     if (modalOpen === i) {
@@ -107,11 +112,11 @@ const GalleryMain = () => {
           ></Image>
         </div>
 
-        <div className={Styles.GoldenOption}>
+        {/* <div className={Styles.GoldenOption}>
           <Image width={24} height={24} src={"/images/gallery/goldbean.png"} />
           <h3>Golden Mode</h3>
-          {/* <Switch {...label} /> */}
-        </div>
+          <Switch {...label} />
+        </div> */}
 
         <div className={Styles.SearcOption}>
           <Image width={24} height={24} src={"/images/gallery/Search.png"} />
@@ -149,7 +154,11 @@ const GalleryMain = () => {
                 {modalOpen === i ? (
                   <div className={Styles.options}>
                     {data.options.map((option) => {
-                      return <p onClick={() => handleFilter(data,option)}>{option}</p>;
+                      return (
+                        <p onClick={() => handleFilter(data, option)}>
+                          {option}
+                        </p>
+                      );
                     })}
                   </div>
                 ) : null}
@@ -157,7 +166,9 @@ const GalleryMain = () => {
             );
           })}
         </div>
-        <button className={Styles.resetButton} onClick={() => handleReset()}>Reset</button>
+        <button className={Styles.resetButton} onClick={() => handleReset()}>
+          Reset
+        </button>
       </div>
 
       <div className={`col-md-9 ${Styles.gallaryRight}`}>
@@ -168,7 +179,7 @@ const GalleryMain = () => {
         </div>
         <div className={Styles.galleryItem}>
           {galleryData.map((data, i) => {
-            const imageLink = data.image.slice(7,data.image.length)
+            const imageLink = data.image.slice(7, data.image.length);
             return (
               <div className={Styles.galleryImages} key={i}>
                 <Image
